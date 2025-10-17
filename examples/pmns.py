8a09344ea3c52beb884709df045ecf58cbf45f16
Adrien Blanchet <adrien.blanchet@live.fr>
2025-10-17 23:33:07 +0200

    example with sterile

--- examples/pmns.py
+++ examples/pmns.py
@@ -7,3 +7,19 @@ phases = {(1,3): np.deg2rad(195)}
 pmns = MixingParameters(dim=3, mixing_angles=angles, dirac_phases=phases)
 U = pmns.U()
 print(np.round(U,3))
+
+
+# example with a sterile state
+angles[(1,4)] = np.arcsin(np.sqrt(0.1))
+# angles[(2,4)] = 0
+# angles[(3,4)] = 0
+
+# two additional dirac phases
+# phases[(1,4)] = np.deg2rad(90) # \delta_{14}
+# phases[(2,4)] = np.deg2rad(0) # \delta_{24}
+
+pmns_3p1 = MixingParameters(dim=4, mixing_angles=angles, dirac_phases=phases)
+U = pmns_3p1.U()
+print("U (3+1):")
+print(np.round(U,3))
+
