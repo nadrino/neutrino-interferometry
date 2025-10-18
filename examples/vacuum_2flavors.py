43944ffe4b3e762667bf30b9b7119cbc2a4a3329
Adrien Blanchet <adrien.blanchet@live.fr>
2025-10-18 16:49:54 +0200

    adding oscillation amplitude

--- examples/vacuum_2flavors.py
+++ examples/vacuum_2flavors.py
@@ -6,7 +6,8 @@ from nu_waves.propagation.oscillator import VacuumOscillator
 import nu_waves.utils.flavors as flavors
 
 # sterile test
-angles = {(1, 2): np.deg2rad(10)}
+osc_amplitude = 0.1 # sin^2(2\theta)
+angles = {(1, 2): np.arcsin(np.sqrt(osc_amplitude))/2}
 pmns = Mixing(dim=2, mixing_angles=angles)
 U_pmns = pmns.get_mixing_matrix()
 print(np.round(U_pmns, 3))
@@ -21,11 +22,12 @@ m2_diag = np.diag(spec.get_m2())
 
 osc = VacuumOscillator(mixing_matrix=U_pmns, m2_list=spec.get_m2())
 
+E_fixed = 5E-3
 L_min, L_max = 1e-3, 20e-3
 L_list = np.linspace(L_min, L_max, 200)
 print(L_list)
 P = osc.probability(
-    L_km=L_list, E_GeV=3E-3,
+    L_km=L_list, E_GeV=E_fixed,
     alpha=flavors.electron,
     beta=flavors.electron, # muon could be sterile
     antineutrino=True
@@ -42,7 +44,7 @@ plt.plot(L_list*1000, [1]*len(L_list), "--", label="Total probability", lw=1.5)
 
 plt.xlabel(r"$L_\nu$ [m]")
 plt.ylabel(r"Probability")
-plt.title(r"eV$^2$ sterile with $E_\nu$ = 3 MeV")
+plt.title(f"eV$^2$ sterile with $E_\\nu$ = {E_fixed*1000} MeV")
 # plt.xlim(L_min, L_max)
 plt.ylim(0, 1.05)
 plt.legend()
