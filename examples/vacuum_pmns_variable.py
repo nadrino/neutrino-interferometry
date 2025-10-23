4cf8ca2b424ce5217b4051a7effc5979d363211a
Adrien Blanchet <adrien.blanchet@live.fr>
2025-10-23 23:11:42 +0200

    adapting vacuum_pmns_variable.py

--- examples/vacuum_pmns_variable.py
+++ examples/vacuum_pmns_variable.py
@@ -2,23 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from nu_waves.models.mixing import Mixing
 from nu_waves.models.spectrum import Spectrum
-from nu_waves.propagation.oscillator import Oscillator
+from nu_waves.hamiltonian.vacuum import VacuumHamiltonian
+from nu_waves.propagation.new_oscillator import Oscillator
 import nu_waves.utils.flavors as flavors
 import nu_waves.utils.style
 
+from nu_waves.globals.backend import Backend
+import torch
+Backend.set_api(torch, device='mps')
+
+
 angles = {(1, 2): np.deg2rad(33.4), (1, 3): np.deg2rad(8.6), (2, 3): np.deg2rad(49)}
 phases = {(1, 3): np.deg2rad(195)}
+dm2 = {(2, 1): 7.42e-5, (3, 2): 0.0024428}
 
-osc = Oscillator(
+h = VacuumHamiltonian(
     mixing_matrix=Mixing(dim=3, mixing_angles=angles, dirac_phases=phases).get_mixing_matrix(),
-    m2_list=Spectrum(n=3, m_lightest=0., dm2={(2, 1): 7.42e-5, (3, 2): 0.0024428}).get_m2()
+    m2_array=Spectrum(n=3, m_lightest=0., dm2=dm2).get_m2()
 )
+osc = Oscillator(hamiltonian=h)
 
 E_min, E_max = 0.2, 3.0
 Enu_list = np.linspace(E_min, E_max, 200)
 P_no = osc.probability(L_km=295, E_GeV=Enu_list, flavor_emit=flavors.muon, flavor_det=flavors.electron)
 
-osc.set_parameters(m2_list=Spectrum(n=3, m_lightest=0., dm2={(2, 1): 7.42e-5, (3, 2): -0.0024428}).get_m2())
+dm2 = {(2, 1): 7.42e-5, (3, 2): -0.0024428}
+h.set_m2_array(Spectrum(n=3, m_lightest=0., dm2=dm2).get_m2())
 P_io = osc.probability(L_km=295, E_GeV=Enu_list, flavor_emit=flavors.muon, flavor_det=flavors.electron)
 
 # Add plotting code
